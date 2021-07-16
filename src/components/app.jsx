ec3b378afff7bf2b72e648386122c77f8dd302ef
James Wordie <james.wordie@yahoo.com>
2021-07-16 11:29:28 +0100

    setup completed for fontawesome

---
+++ src/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+const App = () => {
+  return <FontAwesomeIcon icon={faCoffee} />;
+};
+
+export default App;
